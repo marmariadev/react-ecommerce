0e88aee0dc55535538b15b4053303d1fef56492f
Maria M <marmariadev@gmail.com>
2022-08-10 13:48:00 -0300

    Create Home Component

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <div className="hero">
+            <div class="card text-bg-dark border-0">
+                <img src="/assets/woman.jpg" class="card-img" alt="Woman" height="550px" />
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <div className="container">
+                        <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON</h5>
+                        <p class="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
